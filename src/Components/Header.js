dab0ede40bbb4dde75d1f07eaf112451deb07e7a
Israel Alves <ysrael.2009@hotmail.com>
2022-04-11 21:18:52 -0300

    feat(header): first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Where in the world?</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
