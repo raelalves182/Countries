70a4cd0a0fcfcf90a1c999ee7287118dd76a2db8
Israel Alves <ysrael.2009@hotmail.com>
2022-04-15 18:58:06 -0300

    feat(header): darkmode

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import DarkMode from '../DarkMode/Drakmode';
 
 const Header = () => {
   return (
     <header className="header-main">
       <div className="container">
         <div className="row">
-          <div className="col-xs-6 col-md-3">
-           <h1>Where in the world?</h1>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div className="menu">
+              <h1>Where in the world?</h1>
+              <DarkMode />
+            </div>
           </div>
         </div>
       </div>
