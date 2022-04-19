aacaaf87e0d871a84681fec5ea73284ee1491058
Israel Alves <ysrael.2009@hotmail.com>
2022-04-18 23:51:23 -0300

    feat(header): grid

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,18 +1,128 @@
 import React from 'react'
 import Germany from '../../Assets/germany.jpg';
+import UnitedState from '../../Assets/estados-unidos.jpg';
 
 const Card = () => {
   return (
-    <div className="card">
-      <img src={Germany} alt="Germany" />
-      <div className="body">
-        <h2>Germany</h2>
-        <div className="description">
-          <ul>
-            <li><span>Population:</span> 81,770,900</li>
-            <li><span>Region:</span> Europe</li>
-            <li><span>Capital:</span> Berlin</li>
-          </ul>
+    <div className="row">
+      <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+        <div className="card">
+          <img src={Germany} alt="Germany" />
+          <div className="body">
+            <h2>Germany</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 81,770,900</li>
+                <li><span>Region:</span> Europe</li>
+                <li><span>Capital:</span> Berlin</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+        <div className="card">
+          <img src={UnitedState} alt="Estados Unidos" />
+          <div className="body">
+            <h2>United States od America</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Region:</span> Americas</li>
+                <li><span>Capital:</span> Washington D.C.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+        <div className="card">
+          <img src={UnitedState} alt="Estados Unidos" />
+          <div className="body">
+            <h2>United States od America</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Region:</span> Americas</li>
+                <li><span>Capital:</span> Washington D.C.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+        <div className="card">
+          <img src={UnitedState} alt="Estados Unidos" />
+          <div className="body">
+            <h2>United States od America</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Region:</span> Americas</li>
+                <li><span>Capital:</span> Washington D.C.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+        <div className="card">
+          <img src={UnitedState} alt="Estados Unidos" />
+          <div className="body">
+            <h2>United States od America</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Region:</span> Americas</li>
+                <li><span>Capital:</span> Washington D.C.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+        <div className="card">
+          <img src={UnitedState} alt="Estados Unidos" />
+          <div className="body">
+            <h2>United States od America</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Region:</span> Americas</li>
+                <li><span>Capital:</span> Washington D.C.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+        <div className="card">
+          <img src={UnitedState} alt="Estados Unidos" />
+          <div className="body">
+            <h2>United States od America</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Region:</span> Americas</li>
+                <li><span>Capital:</span> Washington D.C.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <div className="card">
+          <img src={UnitedState} alt="Estados Unidos" />
+          <div className="body">
+            <h2>United States od America</h2>
+            <div className="description">
+              <ul>
+                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Region:</span> Americas</li>
+                <li><span>Capital:</span> Washington D.C.</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
