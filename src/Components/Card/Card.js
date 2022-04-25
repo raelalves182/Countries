21ab3f58f219400b9c11ae5dffcab386991aa9c5
Israel Alves <ysrael.2009@hotmail.com>
2022-04-25 15:43:29 -0300

    feat(header): bandeiras

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import Germany from '../../Assets/germany.jpg';
-import UnitedState from '../../Assets/estados-unidos.jpg';
 
 const Card = () => {
   return (
     <div className="row">
       <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
         <div className="card">
-          <img src={Germany} alt="Germany" />
+          <img src={require('../../Assets/germany.jpg')} alt="Germany" />
           <div className="body">
             <h2>Germany</h2>
             <div className="description">
@@ -22,7 +20,7 @@ const Card = () => {
       </div>
       <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
         <div className="card">
-          <img src={UnitedState} alt="Estados Unidos" />
+          <img src={require('../../Assets/estados-unidos.jpg')} alt="Estados Unidos" />
           <div className="body">
             <h2>United States od America</h2>
             <div className="description">
@@ -37,14 +35,14 @@ const Card = () => {
       </div>
       <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
         <div className="card">
-          <img src={UnitedState} alt="Estados Unidos" />
+          <img src={require('../../Assets/brasil.jpg')} alt="Brasil" />
           <div className="body">
-            <h2>United States od America</h2>
+            <h2>Brazil</h2>
             <div className="description">
               <ul>
-                <li><span>Population:</span> 323,947,000</li>
+                <li><span>Population:</span> 206,135,893</li>
                 <li><span>Region:</span> Americas</li>
-                <li><span>Capital:</span> Washington D.C.</li>
+                <li><span>Capital:</span> Brasília</li>
               </ul>
             </div>
           </div>
@@ -52,14 +50,14 @@ const Card = () => {
       </div>
       <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
         <div className="card">
-          <img src={UnitedState} alt="Estados Unidos" />
+          <img src={require('../../Assets/iceland.jpg')} alt="Iceland" />
           <div className="body">
-            <h2>United States od America</h2>
+            <h2>Iceland</h2>
             <div className="description">
               <ul>
-                <li><span>Population:</span> 323,947,000</li>
-                <li><span>Region:</span> Americas</li>
-                <li><span>Capital:</span> Washington D.C.</li>
+                <li><span>Population:</span> 334,300</li>
+                <li><span>Region:</span> Europe</li>
+                <li><span>Capital:</span> Reykjavík</li>
               </ul>
             </div>
           </div>
@@ -67,14 +65,14 @@ const Card = () => {
       </div>
       <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
         <div className="card">
-          <img src={UnitedState} alt="Estados Unidos" />
+          <img src={require('../../Assets/afghanistan.jpg')} alt="Afghanistan" />
           <div className="body">
-            <h2>United States od America</h2>
+            <h2>Afghanistan</h2>
             <div className="description">
               <ul>
-                <li><span>Population:</span> 323,947,000</li>
-                <li><span>Region:</span> Americas</li>
-                <li><span>Capital:</span> Washington D.C.</li>
+                <li><span>Population:</span> 27,657,145</li>
+                <li><span>Region:</span> Asia</li>
+                <li><span>Capital:</span> Kabul</li>
               </ul>
             </div>
           </div>
@@ -82,14 +80,14 @@ const Card = () => {
       </div>
       <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
         <div className="card">
-          <img src={UnitedState} alt="Estados Unidos" />
+          <img src={require('../../Assets/finland.jpg')} alt="Finland" />
           <div className="body">
-            <h2>United States od America</h2>
+            <h2>Aland Islands</h2>
             <div className="description">
               <ul>
-                <li><span>Population:</span> 323,947,000</li>
-                <li><span>Region:</span> Americas</li>
-                <li><span>Capital:</span> Washington D.C.</li>
+                <li><span>Population:</span> 28,875</li>
+                <li><span>Region:</span> Europe</li>
+                <li><span>Capital:</span> Mariehamn</li>
               </ul>
             </div>
           </div>
@@ -97,14 +95,14 @@ const Card = () => {
       </div>
       <div className="col-xs-12 col-sm-4 col-md-6 col-lg-4">
         <div className="card">
-          <img src={UnitedState} alt="Estados Unidos" />
+          <img src={require('../../Assets/albania.jpg')} alt="Albania" />
           <div className="body">
-            <h2>United States od America</h2>
+            <h2>Albania</h2>
             <div className="description">
               <ul>
-                <li><span>Population:</span> 323,947,000</li>
-                <li><span>Region:</span> Americas</li>
-                <li><span>Capital:</span> Washington D.C.</li>
+                <li><span>Population:</span> 2,886,026</li>
+                <li><span>Region:</span> Europe</li>
+                <li><span>Capital:</span> Tirana</li>
               </ul>
             </div>
           </div>
@@ -112,14 +110,14 @@ const Card = () => {
       </div>
       <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
         <div className="card">
-          <img src={UnitedState} alt="Estados Unidos" />
+          <img src={require('../../Assets/algeria.jpg')} alt="Algeria" />
           <div className="body">
-            <h2>United States od America</h2>
+            <h2>Algeria</h2>
             <div className="description">
               <ul>
-                <li><span>Population:</span> 323,947,000</li>
-                <li><span>Region:</span> Americas</li>
-                <li><span>Capital:</span> Washington D.C.</li>
+                <li><span>Population:</span> 40,400,000</li>
+                <li><span>Region:</span> Africa</li>
+                <li><span>Capital:</span> Algiers</li>
               </ul>
             </div>
           </div>
